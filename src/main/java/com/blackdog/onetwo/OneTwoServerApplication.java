a9a9551aecc3da55566c33c9aade8ffc3fbc86ea
woosunghwan <wsh0821@station3.co.kr>
2021-12-05 11:18:55 +0900

    Initial commit

---
+++ src/main/java/com/blackdog/onetwo/OneTwoServerApplication.java
@@ -0,0 +1,13 @@
+package com.blackdog.onetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTwoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTwoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackdog/onetwo/OneTwoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackdog.onetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTwoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
